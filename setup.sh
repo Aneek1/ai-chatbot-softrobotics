fa37feb13dd419524b743a552a0d96e2fa01bd34
Aneek1 <40447609+Aneek1@users.noreply.github.com>
2025-03-31 13:20:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ source venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 python3 -m spacy download en_core_web_sm
-echo "Setup complete! Run the chatbot with: source venv/bin/activate && python chatbot.py"
+echo "Setup complete! Run the chatbot with: source venv/bin/activate && python3.11 oop_gui_chatbot.py"
